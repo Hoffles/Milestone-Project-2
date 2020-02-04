e6eeffd9f5bfc53b14ccaa785eedf88abffc606b
Hoffles <denisehofler@gmail.com>
2020-02-04 15:29:24 +0000

    Styling

---
+++ recipe.js
@@ -0,0 +1,16 @@
+var data = null;
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = false;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
+
+xhr.open("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=apples%252Cflour%252Csugar");
+xhr.setRequestHeader("x-rapidapi-host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "5fab204894msh2cfc1d601e43969p15226djsn5cedf9554b53");
+
+xhr.send(data);
